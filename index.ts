654f2ab8f263093f647c22bf09d87250fcc44d69
tomas3485 <tomaslozanomena@gmail.com>
2025-10-24 13:21:53 +0200

    parcial 1

--- index.ts
+++ index.ts
@@ -25,93 +25,85 @@ const errorHandler = (
     .json({ error: "Error interno del servidor", detail: err.message });
 };
 
-
-type Team = {
-
- id: number
-
- name: string
-
- city: string
-
- titles: number
-
+type LD = {
+id: number
+lengthMinutes : number
+filmName: string
+region : string
+rotationType : "CAV" | "CLV"
+videoFormat : "NTSC" | "PAL"
 }
 
-let teams: Team[] = [
+let discos : LD []= [
+    {id:1,lengthMinutes:120,filmName:"Pelicula 1",region:"España",rotationType:"CAV",videoFormat:"NTSC"},
+    {id:2,lengthMinutes:240,filmName:"Pelicula 2",region:"Portugal",rotationType:"CLV",videoFormat:"PAL"},
+]
 
- { id: 1, name: "Lakers", city: "Los Angeles", titles: 17 },
+app.get("/ld" ,((req,res)=>{
+    res.status(200).json(discos);
+}))
 
- { id: 2, name: "Celtics", city: "Boston", titles: 17 },
-
-];
-
-
-app.get("/teams",(req,res)=>{
-    res.status(200).json(teams);//status 200 es que esta bien. json teams devuelve teams
-})
-
-
-app.get("/teams/:id",(req,res)=>{
-    const id = Number (req.params.id);//coge el id que pasa el usuario
-    const equipobuscado = teams.find((x)=>{
+app.get("/ld/:id",(req,res)=>{
+    const id = Number (req.params.id);
+    const cdBuscado = discos.find((x)=>{
         return x.id===id;
     })
-    if(!equipobuscado){
-      res.status(404).json({ message: "Equipo no encontrado" })
+    if(!cdBuscado){
+        res.status(404).json({ message :"Disco no encontrado"})
     }else{
-      res.status(200).json(equipobuscado)
+        res.status(200).json(cdBuscado)
     }
 })
 
-app.post("/teams",(req,res)=>{
-    const ultimoequipo = teams.at(-1);
-    const nuevoId = ultimoequipo ? ultimoequipo.id+1 : 0;
 
-    const nuevoEquipo = {
+app.post("/ld",(req,res)=>{
+    const ultimoDisco = discos.at(-1);
+    const nuevoId = ultimoDisco ? ultimoDisco.id+1 : 0;
+
+    const nuevoDisco = {
         id : nuevoId,
         ...req.body
     }
-    teams.push(nuevoEquipo);
-    console.log(nuevoEquipo);
-    res.status(200).json({message: "Equipo creado"});
 
+    discos.push(nuevoDisco)
+    console.log(nuevoDisco)
+    res.status(201).json({message : "CD creado"})
 })
 
-app.delete("/teams/:id",(req,res)=>{
-  const id = Number (req.params.id);//coge el id que pasa el usuario
-    const equipobuscado = teams.find((x)=>{
+app.delete("/ld/:id",(req,res)=>{
+    const id = Number (req.params.id);
+    const discoBuscado = discos.find((x)=>{
         return x.id===id;
     })
-    if(!equipobuscado){
-      res.status(404).json({ message: "Equipo no encontrado" })
+    if(!discoBuscado){
+      res.status(404).json({ message: "Disco no encontrado" })
     }else{
-      teams = teams.filter((x)=>{
+      discos = discos.filter((x)=>{
         return x.id!==id;
       })
-      res.status(200).json({message : "Equipo borrado"})
+      res.status(200).json({message : "Disco borrado"})
     }
-
 })
-//arrancar servidor 
+
 app.listen(3000, () => console.log("Servidor en http://localhost:3000"));
 
-const testApi = async() => {
+const testApi = async () => {
 
-  setTimeout(() => {
-        console.log("Han pasado 2 segundos");
-    }, 2000);
+    setTimeout(() => {
+        console.log("Ha pasado 1 segundo");
+    }, 1000);
+    
+    await axios.get("http://localhost:3000/ld").then((res)=>console.log(res.data));
+    
+    await axios.post("http://localhost:3000/ld",{lengthMinutes:200,filmName:"Pelicula 3",region:"Italia",rotationType:"CLV",videoFormat:"PAL"}
+    ).then((res)=>console.log(res.data));
 
-  await axios.get("http://localhost:3000/teams").then((res)=>console.log(res.data));
+    await axios.get("http://localhost:3000/ld").then((res)=>console.log(res.data));
+    
+    /*
+    await axios.delete("http://localhost:3000/ld/2").then((res)=>console.log(res.data));
 
-  await axios.post("http://localhost:3000/teams",{ name: "Bulls", city: "Chicago", titles: 6 } 
-  ).then((res)=>console.log(res.data));
-
-  await axios.get("http://localhost:3000/teams").then((res)=>console.log(res.data));
-
-  await axios.delete("http://localhost:3000/teams/3").then((res)=>console.log(res.data));
-
-  await axios.get("http://localhost:3000/teams").then((res)=>console.log(res.data));
+    await axios.get("http://localhost:3000/ld").then((res)=>console.log(res.data));
+    */
 }
-testApi();
-console.log("hola")
+testApi()
